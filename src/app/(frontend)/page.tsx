c732b3557771da6d9ba9de2190b26c37fbb5a452
jordan-furr <jordanfurr98@gmail.com>
2025-09-13 14:09:13 -0400

    error

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -8,14 +8,14 @@ export default async function Home() {
   const posts = await client.fetch(POSTS_QUERY, {}, options);
 
   return (
-    <main className="container mx-auto grid grid-cols-1 gap-6 p-12">
+    <main className="homeContainer">
       <h1>drink-wall</h1>
-      <h3 className="text-4xl font-bold">Fresh Finds</h3>
-      <ul className="grid grid-cols-1 divide-y divide-blue-100">
+      <h3>Fresh Finds</h3>
+      <ul className="">
         {posts.map((post) => (
           <li key={post._id}>
             <Link
-              className="block p-4 hover:text-blue-500"
+              className="under-hov"
               href={`/posts/${post?.slug?.current}`}
             >
               {post?.title}
