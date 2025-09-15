b09b1c155ea51d24538784d7d86dea736af55690
jordan-furr <jordanfurr98@gmail.com>
2025-09-14 22:35:20 -0400

    40 posts

--- src/sanity/lib/queries.ts
+++ src/sanity/lib/queries.ts
@@ -1,7 +1,7 @@
 import { defineQuery } from 'next-sanity'
 
 export const POSTS_QUERY =
-  defineQuery(`*[_type == "post" && defined(slug.current)]|order(publishedAt desc)[0...24]{
+  defineQuery(`*[_type == "post" && defined(slug.current)]|order(publishedAt desc)[0...40]{
   _id,
   title,
   slug,
